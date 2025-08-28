c58bda7adfba4fedad88465dcd6abe06039302f1
Luke McDonald <lamcd94@gmail.com>
2025-08-28 15:05:30 -0500

    Why gitignore not working?

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -9,29 +9,21 @@
                 <p class="mt-1 text-sm/6 text-gray-600">We just need a handful of details from you.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title" >Title</x-form-label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="title" type="text" name="title" placeholder="Fireman" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
-                            </div>
-                            @error('title')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-input id="title" name="title" placeholder="Fireman" required></x-form-input>
+                            <x-form-error name="title"/>
                         </div>
-                    </div>
+                    </x-form-field>
 
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="salary" type="text" name="salary" placeholder="$50,000 Per Year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
-                            </div>
-                           @error('salary')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-input id="salary" name="salary" placeholder="$50,000" required/>
+                            <x-form-error name="salary"/>
                         </div>
-                    </div>
+                    </x-form-field>
                     
 
                     
